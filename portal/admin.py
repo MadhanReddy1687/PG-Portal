6578dff72b1ea58b5de9afe83266cd91ff6c10b7
MadhanReddy1687 <87870529+MadhanReddy1687@users.noreply.github.com>
2021-07-25 17:24:35 +0530

    Add files via upload

---
+++ portal/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import City,Area,Pg,Images,Sharing
+# Register your models here.
+admin.site.register(City)
+admin.site.register(Area)
+admin.site.register(Pg)
+admin.site.register(Images)
+admin.site.register(Sharing)
