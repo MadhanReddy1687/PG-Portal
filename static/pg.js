100d9a7515582780ea35a028588b1533ab34353d
notyetgrounded <69687609+notyetgrounded@users.noreply.github.com>
2021-07-25 17:41:09 +0530

    Add files via upload

---
+++ static/pg.js
@@ -0,0 +1,18 @@
+function onGo() {
+  document.getElementById("types").style.display = "block";
+  document.getElementById("welcomeImg").style.display = "none";
+}
+function onGetDetails() {
+  document.getElementById("showtable").style.display = "block";
+}
+function onReset() {
+  document.getElementById("types").style.display = "none";
+  document.getElementById("welcomeImg").style.display = "block";
+}
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
